2d63ed1234cdae9115e82ca400897260ae04c467
sunnycharkhwal1 <114386935+maxmover@users.noreply.github.com>
2022-09-26 14:37:13 +0530

    1

--- src/components/common/RightNavbar/Submenus/backdrop.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./backdrop.scss";
-
-const BackDrop = (props) => {
-  return <div className="backdrop" onClick={props.closeDrawer}></div>;
-};
-
-export default BackDrop;
