7eb43fa954e96619eda8cef5d322e9790856bb69
a <114386935+maxmover@users.noreply.github.com>
2022-09-28 08:52:57 +0530

    new

--- src/components/common/btn/Btn.jsx
+++ src/components/common/btn/Btn.jsx
@@ -1,7 +1,48 @@
+import { NavLink } from "react-router-dom";
 export const BluieBtn = () => {
   return (
     <>
       <h1>blue btn</h1>
     </>
   );
+};
+export const OutlineBtn = (props) => {
+  const OutlineBtnNormal = () => {
+    return (
+      <>
+        <button onClick={props.onClick} className="out_line_btn">
+          {props.titel}
+        </button>
+      </>
+    );
+  };
+  const OutlineBtnIcon = () => {
+    return (
+      <button className="out_line_btn">
+        <span>{props.icon}</span>
+        {props.title}
+      </button>
+    );
+  };
+  return props.icon ? <OutlineBtnIcon /> : <OutlineBtnNormal />;
+};
+export const OutlineLinkBtn = (props) => {
+  const OutlineBtnNormal = () => {
+    return (
+      <>
+        <NavLink to={props.link} className="out_line_btn">
+          {props.titel}
+        </NavLink>
+      </>
+    );
+  };
+  const OutlineBtnIcon = () => {
+    return (
+      <NavLink to={props.link} className="out_line_btn">
+        <span>{props.icon}</span>
+        {props.title}
+      </NavLink>
+    );
+  };
+  return props.icon ? <OutlineBtnIcon /> : <OutlineBtnNormal />;
 };
--- src/pages/sunny/ServiceCategories.jsx
+++ src/pages/sunny/ServiceCategories.jsx
@@ -1,11 +1,14 @@
 import { ActionStrip } from "../../components/common/actionStrip/ActionStrip";
 import { BsPlusLg } from "react-icons/bs";
+import { OutlineBtn, OutlineLinkBtn } from "../../components/common/btn/Btn";
 export const ServiceCategories = () => {
   return (
     <>
       <div className="custom_container">
         <h1>ServiceCategories</h1>
         <ActionStrip title="dfjfvbd" icon={<BsPlusLg />} />
+        <OutlineBtn title="dfjfvbd" icon={<BsPlusLg />} />
+        <OutlineLinkBtn title="dfjfvbd" icon={<BsPlusLg />} link="/" />
       </div>
     </>
   );
